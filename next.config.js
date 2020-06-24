221b91a412d6753469ab89bf68236c453ea2d2d8
kanmadozo <trakyrichardkanm3@gmail.com>
2020-06-24 14:57:47 +0100

    Add Export Confirmation

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-
+// must restart server whenever you make changes in next.config
+export const env = {
+  MONGO_SRV:
+    "mongodb+srv://Traky:Trakouss@shoppingapp-h4s3y.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "Trakouss",
+  CLOUDINARY_URL:
+    "https://api.cloudinary.com/v1_1/ecole-superieur-des-t-l-communications/image/upload",
+  STRIPE_SECRET_KEY:
+    "sk_test_51GvHSoB0kYNpU0sqGJsUIt7ZGkgxxFc7RvttOMguWt9SnpcJU6Qgc9afqfpAPavFDQd3DIMCaNzXjtEKQxrm3aKj00bEK1YCEk"
+};
